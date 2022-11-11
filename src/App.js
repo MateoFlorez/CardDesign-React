4e888382aea821ec878aa2782916b388f8751ae5
Mateo Guisao Flórez <1039475395@itagui.loc>
2022-11-11 15:51:10 -0500

    Organizo componente RightSide

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import SideMenu from './components/SideMenu'
-import InfoCard from './components/InfoCard'
-import RightSide from './components/RightSide'
-import OrangeBox from './components/OrangeBox'
-import {Routes, Route} from "react-router-dom"
-
+import SideMenu from "./components/SideMenu";
+import InfoCard from "./components/InfoCard";
+import RightSide from "./components/RightSide";
+import OrangeBox from "./components/OrangeBox";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className='h-screen w-full overflow-hidden flex justify-center box-border bg-zinc-300'>
-      <OrangeBox/>
-      <SideMenu /> 
+    <div className="h-screen w-full overflow-hidden flex justify-center box-border bg-zinc-300">
+      <OrangeBox />
+      <SideMenu />
       <Routes>
-        <Route path='/billing' element={<div className='w-3/5 flex z-20'><InfoCard /> <RightSide/></div>}/>
+        <Route
+          path="/billing"
+          element={
+            <div className="w-3/5 flex z-20">
+              <InfoCard /> <RightSide />
+            </div>
+          }
+        />
       </Routes>
-      {/* <InfoCard/>
-      <LeftSide/> */}
     </div>
   );
 }
--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -1,11 +1,11 @@
 export default function RightSide() {
   return (
     <div className="bg-slate-100 w-72 mt-20 h-4/5 flex justify-center px-7 rounded-r-2xl">
-      <div className="bg-orange-500 h-44 mt-28 rounded-2xl w-full p-4 text-white text-center">
+      <div className="bg-orange-500 h-44 mt-28 rounded-2xl w-52 p-4 text-white text-center">
         <p className="text-sm">Your plan</p>
         <p className="font-bold text-2xl mt-2">Pro Annual</p>
         <p className="text-xs mt-2">Renews on Nov.2021</p>
-        <button className="border border-white rounded-md h-8 mt-4">
+        <button className="w-fit font-semibold px-1 border border-white rounded-md h-8 mt-4">
           Cancel subscription
         </button>
       </div>
